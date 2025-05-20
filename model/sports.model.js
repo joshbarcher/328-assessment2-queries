537027aa0bdd1897af46e0da9472232daf5d08d1
jarcher <jarcher@greenriver.edu>
2025-05-20 00:20:01 -0700

    Added model file.

---
+++ model/sports.model.js
@@ -0,0 +1,19 @@
+import { DataTypes } from 'sequelize';
+import sequelize from './../db/connect.js';
+
+const schema = sequelize.define('sport', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: DataTypes.STRING,
+    teamBased: DataTypes.BOOLEAN,
+    popularityRank: DataTypes.INTEGER,
+    averageDuration: DataTypes.INTEGER, // in minutes
+    olympicSport: DataTypes.BOOLEAN
+});
+
+schema.sync({ force: true });
+
+export default schema;
